cf7baf3b0a2d4e19cd65c9b9fc23863befe9ecb1
Iva Miholic <ivamiho@gmail.com>
2015-10-20 18:39:06 +0200

    Adjust placeholders and map default view

--- static/js/mape.js
+++ static/js/mape.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var map = new L.Map('map');
-    map.setView(new L.LatLng(45.8150, 15.9819), 8);
+    map.setView(new L.LatLng(45.8150, 15.9819), 10);
     var googleLayer = new L.Google('ROADMAP');
     map.addLayer(googleLayer);
     //map.attributionControl.setPrefix('');
