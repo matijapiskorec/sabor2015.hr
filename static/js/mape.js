345aa8738588a2e5ee5799568a773239a0cc3148
Iva Miholic <ivamiho@gmail.com>
2015-10-26 15:50:56 +0100

    Added labels to map of Zagreb

--- static/js/mape.js
+++ static/js/mape.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
     //map.attributionControl.setPrefix('');
     var kmlLayer = new L.KML("../data/karta_zagreb.kml", {async: true});
     map.addLayer(kmlLayer);
-    });
+
+    var markerA = new L.Marker([45.870282, 15.94305], {opacity: 0.01});
+    markerA.bindLabel(
+      "I", {noHide: true, className: "map-label", offset: [0, 0] });
+    markerA.addTo(map);
+
+    var markerB = new L.Marker([45.89282, 16.10305], {opacity: 0.01});
+    markerB.bindLabel(
+      "II", {noHide: true, className: "map-label", offset: [0, 0] });
+    markerB.addTo(map);
+
+    var markerC = new L.Marker([45.81282, 16.03305], {opacity: 0.01});
+    markerC.bindLabel(
+      "VI", {noHide: true, className: "map-label", offset: [0, 0] });
+    markerC.addTo(map);
+
+    var markerD = new L.Marker([45.73282, 15.89], {opacity: 0.01});
+    markerD.bindLabel(
+      "VII", {noHide: true, className: "map-label", offset: [0, 0] });
+    markerD.addTo(map);
+
+});
