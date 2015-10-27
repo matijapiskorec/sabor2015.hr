a6d1b311f8f597c81e59938891256adddf06f857
Matija Piskorec <matija.piskorec@gmail.com>
2015-10-27 19:10:53 +0100

    Animated colorfull circular chart for friend's statistics.

--- static/js/functions.js
+++ static/js/functions.js
@@ -998,6 +998,7 @@ sabor2015.directive('statisticsOfFriendsCircle', function ($parse) {
         var election_regions = newData[0];
 
         var data = friends_data.map(function(d){return {
+          "id": d.list,
           "list": _.pluck(_.where(election_regions,{'list_id':Number(d.list)}),'short_name')[0],
           "votes": d.votes
         };});
@@ -1005,18 +1006,14 @@ sabor2015.directive('statisticsOfFriendsCircle', function ($parse) {
 
         // This could go outside watch but then we have problems with duplication of text labels...
 
-        $(element).html(
-          '<div id="statistics-of-friends-circle"></div>'
-        );
-
         var margin = { top: 20, right: 20, bottom: 20, left: 20 },
             width = 800 - margin.left - margin.right;
         var height = 400 - margin.top - margin.bottom;
         var radius = Math.min(width, height) / 2;
         
-        $("#statistics-of-friends-circle").empty();
-        
-        var svg = d3.select("#statistics-of-friends").append("svg")
+        $(element).empty();
+
+        var svg = d3.select(element[0]).append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g");
@@ -1032,17 +1029,18 @@ sabor2015.directive('statisticsOfFriendsCircle', function ($parse) {
 
         var pie = d3.layout.pie()
           .sort(null)
+          .padAngle(0.02)
           .value(function(d) {
             return d.votes;
           });
 
         var arc = d3.svg.arc()
-          .outerRadius(radius * 0.7) // 0.8)
-          .innerRadius(radius * 0.6); // 0.4);
+          .outerRadius(radius * 0.7)
+          .innerRadius(radius * 0.55);
 
         var outerArc = d3.svg.arc()
           .innerRadius(radius * 0.7)
-          .outerRadius(radius * 0.7); // 0.9);
+          .outerRadius(radius * 0.7);
 
         svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
@@ -1070,106 +1068,209 @@ sabor2015.directive('statisticsOfFriendsCircle', function ($parse) {
 
         var data = reduceData(data);
 
-        // TODO: Adding default color ("#fcfbfd") multiple times at the end ensures that color never repeats.
-        //       This could be done smarter. 
+        // Pallete with sequential purple hues and colorfull pallete with specific color for each list
+        var colors = "#f7f7f7"; // white for everyone
+        if (attrs.colors=="sequential") {
+          colors = ["#3f007d","#54278f","#6a51a3","#807dba","#9e9ac8","#bcbddc","#dadaeb","#efedf5","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd"];
+        } else if (attrs.colors=="colorfull") {
+          colors = data.map(function(d){
+            if (d.id==4) return "#d73027"; // red for SDP
+            if (d.id==8) return "#4575b4"; // blue for HDZ
+            if (d.id==3) return "#fee090"; // yellow for Zivi Zid
+            if (d.id==6) return "#1a9850"; // green for Milan Bandic
+            if (d.id==37) return "#f46d43"; // orange for MOST
+            if (d.id==45) return "#a6d96a"; // light green for ORAH
+            return "#f7f7f7"; // white for everyone else
+          });
+        }
+
         var color = d3.scale.ordinal()
           .domain( data.map(function(d){return d.list;}) )
-          .range(["#3f007d","#54278f","#6a51a3","#807dba","#9e9ac8","#bcbddc","#dadaeb","#efedf5","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd","#fcfbfd"]);
-
-        change(data);
+          .range(colors);
         
+        // Start with empty chart then visualize change to full chart
+        changeAnimated([{ list: 'ostali', votes: 1 }],0);
+        changeAnimated(data,4000);
 
-        function change(data) {
 
-          /* ------- PIE SLICES -------*/
+        function mergeWithFirstEqualZero(first, second){
+          var secondSet = d3.set(); second.forEach(function(d) { secondSet.add(d.list); });
+
+          var onlyFirst = first
+            .filter(function(d){ return !secondSet.has(d.list) })
+            .map(function(d) { return {list: d.list, votes: 0}; });
+          return d3.merge([ second, onlyFirst ])
+            .sort(function(a,b) {
+              return d3.ascending(a.list, b.list);
+            });
+        }
+
+        function changeAnimated(data,duration) {
+          var data0 = svg.select(".slices").selectAll("path.slice")
+            .data().map(function(d) { return d.data });
+          if (data0.length == 0) data0 = data;
+          var was = mergeWithFirstEqualZero(data, data0);
+          var is = mergeWithFirstEqualZero(data0, data);
+
+          /* ------- SLICE ARCS -------*/
+
           var slice = svg.select(".slices").selectAll("path.slice")
-            .data(pie(data), key);
+            .data(pie(was), key);
 
           slice.enter()
             .insert("path")
+            .attr("class", "slice")
             .style("fill", function(d) { return color(d.data.list); })
-            .attr("class", "slice");
+            .each(function(d) {
+              this._current = d;
+            });
+
+          slice = svg.select(".slices").selectAll("path.slice")
+            .data(pie(is), key);
 
           slice   
-            .transition().duration(1000)
+            .transition().duration(duration)
             .attrTween("d", function(d) {
-              this._current = this._current || d;
               var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
+              var _this = this;
               return function(t) {
-                return arc(interpolate(t));
+                _this._current = interpolate(t);
+                return arc(_this._current);
               };
-            })
+            });
 
-          slice.exit()
+          slice = svg.select(".slices").selectAll("path.slice")
+            .data(pie(data), key);
+
+          slice
+            .exit().transition().delay(duration).duration(0)
             .remove();
+
+          /* ------- COUNTERS -------*/
+
+          // var sumVotes = function(data) {
+          //   return data.map(function(d){return d.votes})
+          //              .reduce(function(prev,curr){return curr + prev;})
+          //     };
+
+          // var counter = d3.select("#total-votes-friends")
+          //   .data(sumVotes(was));
+
+          // counter = d3.select("#total-votes-friends")
+          //   .data(sumVotes(is));
+
+          // counter.transition().duration(duration)
+          //   .tween("text",
+          //     function (d) {
+          //       var i = d3.interpolate(0, d);
+          //       return function (t) {
+          //         this.textContent = Math.ceil(i(t));
+          //       };
+          //     });
+
+          // var counter = d3.select("#total-votes-friends")
+          //   .data(sumVotes(data));
 
           /* ------- TEXT LABELS -------*/
 
           var text = svg.select(".labels").selectAll("text")
-            .data(pie(data), key);
+            .data(pie(was), key);
 
           text.enter()
             .append("text")
             .attr("dy", ".35em")
+            .style("opacity", 0)
             .text(function(d) {
-              return d.data.list + ' (' + d.data.votes + ')';
+              return d.data.list;
+            })
+            .each(function(d) {
+              this._current = d;
             });
-
+          
           function midAngle(d){
             return d.startAngle + (d.endAngle - d.startAngle)/2;
           }
 
-          text.transition().duration(1000)
+          text = svg.select(".labels").selectAll("text")
+            .data(pie(is), key);
+
+          text.transition().duration(duration)
+            .style("opacity", function(d) {
+              return d.data.votes == 0 ? 0 : 1;
+            })
             .attrTween("transform", function(d) {
-              this._current = this._current || d;
               var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
+              var _this = this;
               return function(t) {
                 var d2 = interpolate(t);
+                _this._current = d2;
                 var pos = outerArc.centroid(d2);
-                pos[0] = 0.85 * radius * (midAngle(d2) < Math.PI ? 1 : -1); // 0.85 finetunes horizontal distance
+                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
                 return "translate("+ pos +")";
               };
             })
             .styleTween("text-anchor", function(d){
-              this._current = this._current || d;
               var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
               return function(t) {
                 var d2 = interpolate(t);
                 return midAngle(d2) < Math.PI ? "start":"end";
               };
-            });
+            })
+            .tween("text",
+              function (d) {
+                var i = d3.interpolate(0, d.data.votes);
+                return function (t) {
+                  this.textContent = d.data.list + ' (' + Math.ceil(i(t)) + ')';
+                };
+              });
+          
+          text = svg.select(".labels").selectAll("text")
+            .data(pie(data), key);
 
-          text.exit()
+          text
+            .exit().transition().delay(duration)
             .remove();
 
           /* ------- SLICE TO TEXT POLYLINES -------*/
 
           var polyline = svg.select(".lines").selectAll("polyline")
-            .data(pie(data), key);
+            .data(pie(was), key);
           
           polyline.enter()
-            .append("polyline");
+            .append("polyline")
+            .style("opacity", 0)
+            .each(function(d) {
+              this._current = d;
+            });
 
-          polyline.transition().duration(1000)
+          polyline = svg.select(".lines").selectAll("polyline")
+            .data(pie(is), key);
+          
+          polyline.transition().duration(duration)
+            .style("opacity", function(d) {
+              return d.data.votes == 0 ? 0 : .5;
+            })
             .attrTween("points", function(d){
-              this._current = this._current || d;
+              this._current = this._current;
               var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
+              var _this = this;
               return function(t) {
                 var d2 = interpolate(t);
+                _this._current = d2;
                 var pos = outerArc.centroid(d2);
-                pos[0] = radius * 0.80 * (midAngle(d2) < Math.PI ? 1 : -1); // 0.80 finetunes horizontal distance
+                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
                 return [arc.centroid(d2), outerArc.centroid(d2), pos];
               };      
             });
           
-          polyline.exit()
+          polyline = svg.select(".lines").selectAll("polyline")
+            .data(pie(data), key);
+          
+          polyline
+            .exit().transition().delay(duration)
             .remove();
-
         };
+
 
       });
 
